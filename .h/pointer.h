ca1ba7a98c28bf2d0dd332ed38421beec22ac580
Shirakami Emmmer <yjhdandan@163.com>
2019-11-29 08:42:13 +0800

    pointer v1.0

---
+++ .h/pointer.h
@@ -0,0 +1,14 @@
+//
+// Created by Shirakami Emmmer on 2019/11/15.
+//
+
+#ifndef CLION_POINTER_H
+#define CLION_POINTER_H
+
+
+class pointer {
+
+};
+
+
+#endif //CLION_POINTER_H
---
+++ pointer.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Shirakami Emmmer on 2019/11/15.
+//
+
+#include ".h/pointer.h"
+#include <iostream>
+#include ".h/printID.h"
+
+int main() {
+    int *arrayX = nullptr;
+    int **arrayY = nullptr;
+
+    return 0;
+}
