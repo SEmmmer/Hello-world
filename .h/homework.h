a3f182c6bffbb589f4480f999df11e38e7f6023d
Shirakami Emmmer <yjhdandan@163.com>
2019-12-06 15:52:37 +0800

    fib -> homework

---
+++ .h/homework.h
@@ -0,0 +1,14 @@
+//
+// Created by Shirakami Emmmer on 2019/11/22.
+//
+
+#ifndef CLION_HOMEWORK_H
+#define CLION_HOMEWORK_H
+
+
+class homework {
+
+};
+
+
+#endif //CLION_HOMEWORK_H
---
+++ homework.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Shirakami Emmmer on 2019/11/22.
+//
+#include ".h/printID.h"
+#include ".h/homework.h"
+#include <iostream>
+#include <iomanip>
+
+
+int main() {
+    printID();
+    return 0;
+}
