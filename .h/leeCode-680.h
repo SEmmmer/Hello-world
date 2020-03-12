6f455e34d638dba7ca4aab251bef5dbf82144432
Shirakami Emmmer <yjhdandan@163.com>
2020-03-12 23:12:12 +0800

    updating

---
+++ .h/leeCode-680.h
@@ -0,0 +1,14 @@
+//
+// Created by Shirakami Emmmer on 2019/11/3.
+//
+
+#ifndef CLION_LEECODE_680_H
+#define CLION_LEECODE_680_H
+
+
+class leeCode {
+
+};
+
+
+#endif //CLION_LEECODE_680_H
---
+++ leeCode-680.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Shirakami Emmmer on 2019/11/3.
+//
+
+#include ".h/leeCode-680.h"
+#include <iostream>
+#include <string>
+
+
+int main() {
+    std::string str ;
+    std::cin >> str;
+    int length = str.length();
+    std::cout << length;
+
+    return 0;
+}
